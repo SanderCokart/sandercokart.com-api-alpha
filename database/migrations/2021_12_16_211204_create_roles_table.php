795a6a8cbbd3e7df9c43fa3bf33b4693c7b04171
Sander Cokart <cokart32@gmail.com>
2022-10-13 23:51:43 +0200

    added indexing for roles

--- database/migrations/2021_12_16_211204_create_roles_table.php
+++ database/migrations/2021_12_16_211204_create_roles_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->unique();
+            $table->id()->index();
+            $table->string('name')->unique()->index();
         });
     }
 
